ce059be9648c0399aa6b41a43094ba59c9691116
Allen Lai <allen_lai@trend.com.tw>
2014-10-02 14:40:03 +0800

    add daily brew cleanup

--- dev/do_update.sh
+++ dev/do_update.sh
@@ -1,9 +1,10 @@
 echo $(date)
 /usr/local/bin/brew update
+/usr/local/bin/brew cleanup
 echo "========= Homebrew: OUTDATED ========="
 /usr/local/bin/brew outdated
 echo "========= python ========="
 /usr/local/bin/pip install --upgrade setuptools pip ipython cython
 /usr/local/bin/pip3 install --upgrade setuptools pip ipython cython
 echo "========= vim ========="
-/usr/local/bin/git pull ~/.vim/bundle/syntastic/ || (mkdir ~/.vim/bundle && cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic.git)
+(cd ~/.vim/bundle/syntastic/; /usr/local/bin/git pull  || (mkdir ~/.vim/bundle && cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic.git))
