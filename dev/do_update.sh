6ec462e5cd05f7dad7d02c258aa8c1d7b35c8e3a
Allen Lai <allen_lai@trend.com.tw>
2014-10-21 17:55:57 +0800

    pip upgrade virtualenv

--- dev/do_update.sh
+++ dev/do_update.sh
@@ -5,7 +5,7 @@ echo $(date)
 echo "========= Homebrew: OUTDATED ========="
 /usr/local/bin/brew outdated
 echo "========= python ========="
-/usr/local/bin/pip install --upgrade setuptools pip ipython cython
-/usr/local/bin/pip3 install --upgrade setuptools pip ipython cython
+/usr/local/bin/pip install --upgrade setuptools pip ipython virtualenv cython
+/usr/local/bin/pip3 install --upgrade setuptools pip ipython virtualenv cython
 echo "========= vim ========="
 (cd ~/.vim/bundle/syntastic/; /usr/local/bin/git pull  || (mkdir ~/.vim/bundle && cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic.git))
