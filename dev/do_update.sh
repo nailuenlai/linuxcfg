e5391e8a0eabb4eb696f2610834e06644e470ac3
Allen Lai <nailuenlai@gmail.com>
2014-11-30 15:53:24 +0800

    fix vim plugin first install

--- dev/do_update.sh
+++ dev/do_update.sh
@@ -8,5 +8,5 @@ echo "========= python ========="
 /usr/local/bin/pip install --upgrade setuptools pip ipython virtualenv cython
 /usr/local/bin/pip3 install --upgrade setuptools pip ipython virtualenv cython
 echo "========= vim ========="
-(cd ~/.vim/bundle/syntastic/; /usr/local/bin/git pull  || (mkdir -p ~/.vim/bundle && cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic.git))
-(cd ~/.vim/bundle/vim-markdown/; /usr/local/bin/git pull  || (mkdir -p ~/.vim/bundle && cd ~/.vim/bundle && git clone https://github.com/plasticboy/vim-markdown.git))
+((cd ~/.vim/bundle/syntastic/ && /usr/local/bin/git pull) || (mkdir -p ~/.vim/bundle && cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic.git))
+((cd ~/.vim/bundle/vim-markdown/ && /usr/local/bin/git pull) || (mkdir -p ~/.vim/bundle && cd ~/.vim/bundle && git clone https://github.com/plasticboy/vim-markdown.git))
