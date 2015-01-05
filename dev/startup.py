1b7d2e996d9655cad81cea3d8c78dcf8dbb4a693
Allen Lai <allen_lai@trend.com.tw>
2015-01-05 17:00:11 +0800

    stdout default to UTF8

--- dev/startup.py
+++ dev/startup.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import sys
 
+sys.setdefaultencoding('utf-8')
+
+#pd.describe_option('display')
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_colwidth', 150)
+pd.set_option('display.width', None)
+pd.set_option('display.max_seq_items', 500)
+pd.set_option('display.mpl_style', 'default')
