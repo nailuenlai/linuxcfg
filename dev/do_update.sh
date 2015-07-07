ae1d902ed6128aeef43ef7e441f15e5110e288ab
Allen Lai <allen_lai@trend.com.tw>
2015-07-07 11:49:50 +0800

    list pip outdated

--- dev/do_update.sh
+++ dev/do_update.sh
@@ -5,8 +5,10 @@ echo $(date)
 echo "========= Homebrew: OUTDATED ========="
 /usr/local/bin/brew outdated
 echo "========= python ========="
-/usr/local/bin/pip install --upgrade setuptools pip ipython virtualenv cython
-/usr/local/bin/pip3 install --upgrade setuptools pip ipython virtualenv cython
+/usr/local/bin/pip2 install --upgrade setuptools pip ipython virtualenv
+/usr/local/bin/pip3 install --upgrade setuptools pip ipython virtualenv
+/usr/local/bin/pip2 list --outdated
+/usr/local/bin/pip3 list --outdated
 echo "========= vim ========="
 ((cd ~/.vim/bundle/syntastic/ && /usr/local/bin/git pull) || (mkdir -p ~/.vim/bundle && cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic.git))
 ((cd ~/.vim/bundle/vim-markdown/ && /usr/local/bin/git pull) || (mkdir -p ~/.vim/bundle && cd ~/.vim/bundle && git clone https://github.com/plasticboy/vim-markdown.git))
