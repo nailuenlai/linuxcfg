47572c923394e786678947045809de4573e1da3c
Allen Lai <allen_lai@trend.com.tw>
2015-07-30 18:15:09 +0800

    script to fix python virtualenv after brew upgrade python

---
+++ dev/bin/fix_virtualenv_after_python_upgrade.sh
@@ -0,0 +1,9 @@
+#find ~/dev/py_env/ -type l -delete
+#virtualenv ~/dev/py_env/ -p python2
+if [[ $# < 3 ]]; then
+    echo "Usage: $0 ~/dev/py_env python2"
+    echo "	$0 ~/dev/py3_env python3"
+    exit 1
+fi
+find "$1" -type l -delete
+virtualenv "$1" -p "$2"
