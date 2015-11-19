33eb3e5b1cc426d43f055c01cf0fcc958ff53a74
Allen Lai <nailuenlai@gmail>
2015-11-20 01:17:09 +0800

    fix off-by-1 bug

--- dev/bin/fix_virtualenv_after_python_upgrade.sh
+++ dev/bin/fix_virtualenv_after_python_upgrade.sh
@@ -1,6 +1,6 @@
 #find ~/dev/py_env/ -type l -delete
 #virtualenv ~/dev/py_env/ -p python2
-if [[ $# < 3 ]]; then
+if [[ $# < 2 ]]; then
     echo "Usage: $0 ~/dev/py_env python2"
     echo "	$0 ~/dev/py3_env python3"
     exit 1
